072c3373fb7defd9a8515a81400c483eed6633c0
Aditya Firoda <adityafiroda@gmail.com>
2020-05-01 14:35:45 +0530

    Init Commit

---
+++ Day1.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(isBadVersion(mid) == true  && (mid == 0 || isBadVersion(mid-1) == false))
+                return mid;
+            else if(isBadVersion(mid) == true)
+                end = mid-1;
+            else
+                start = mid+1;
+        }
+        return -1;
+    }
+};
